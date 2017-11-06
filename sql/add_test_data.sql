95253ddd6d87dc2f9f76ad54b90043dc6851937d
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2017-11-06 20:05:12 +0200

    Insert lauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,25 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO kayttaja (tunnus, salasana) VALUES ('tunnus1', 'salasana');
+INSERT INTO kayttaja (tunnus, salasana) VALUES ('tunnus2', 'salasana');
+
+INSERT INTO askare (teksti, tarkeysaste) VALUES ('hello', 1);
+
+INSERT INTO luokka (nimi, vari) VALUES (
+  'testiluokka1',
+  '#d73a49'
+);
+
+INSERT INTO kayttajaAskare (kayttaja_id, askare_id) VALUES (
+  (SELECT id FROM kayttaja WHERE tunnus='tunnus1'),
+  (SELECT id FROM askare WHERE teksti='hello')
+);
+
+INSERT INTO kayttajaLuokka (kayttaja_id, luokka_id) VALUES (
+  (SELECT id FROM kayttaja WHERE tunnus='tunnus1'),
+  (SELECT id FROM luokka WHERE nimi='testiluokka1')
+);
+
+INSERT INTO askareLuokka (askare_id, luokka_id) VALUES (
+  (SELECT id FROM askare WHERE teksti='hello'),
+  (SELECT id FROM luokka WHERE nimi='testiluokka1')
+);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,34 +1,35 @@
 
-CREATE TABLE kayttaja(
+CREATE TABLE kayttaja (
   id SERIAL PRIMARY KEY,
   tunnus VARCHAR(50) NOT NULL,
   salasana VARCHAR(50) NOT NULL,
-  rekisteroitymisPaiva DATE
+  rekisteroitymisPaiva DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE askare(
+CREATE TABLE askare (
   id SERIAL PRIMARY KEY,
   teksti VARCHAR(400) NOT NULL,
   tarkaysaste INTEGER DEFAULT 0,
-  luontipaiva DATE
+  luontipaiva DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE luokka(
+CREATE TABLE luokka (
   id SERIAL PRIMARY KEY,
-  nimi VARCHAR(50) NOT NULL
+  nimi VARCHAR(50) NOT NULL,
+  vari VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE kayttajaAskare(
+CREATE TABLE kayttajaAskare (
   kayttaja_id INTEGER REFERENCES kayttaja(id),
   askare_id INTEGER REFERENCES askare(id)
 );
 
-CREATE TABLE kayttajaLuokka(
+CREATE TABLE kayttajaLuokka (
   kayttaja_id INTEGER REFERENCES kayttaja(id),
   luokka_id INTEGER REFERENCES luokka(id)
 );
 
-CREATE TABLE askareLuokka(
+CREATE TABLE askareLuokka (
   askare_id INTEGER REFERENCES askare(id),
   luokka_id INTEGER REFERENCES luokka(id)
 );
